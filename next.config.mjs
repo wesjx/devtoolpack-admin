1c60e56dcd79609f9323e113ebc532b8321f0b31
Wesley Junio <wesjrpiri@gmail.com>
2025-07-11 23:44:41 +0100

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// ficheiro: next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // As suas configurações do Next.js vão aqui.
+  // Se não tiver nenhuma configuração customizada, pode deixar este objeto vazio.
+  // Exemplo:
+  // images: {
+  //   domains: ['example.com'],
+  // },
+};
+
+export default nextConfig;
